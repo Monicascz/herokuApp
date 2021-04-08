2c08388cc784d675da38b7adef375df3ed3f0eac
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-04-08 10:55:12 -0700

    monolith push

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 9000
 
+const path = require("path") // comes with Nodejs don't need to install dependency.
+
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "client/build"))) // express.static is a built in function that basically tells it what to load. Then we include what to laod. Here we included the directory name, then go into client, then go into the build folder.
+
 
 app.use("/api/", (_, res)=>{
     res.json({data: "API is accounted for"})
